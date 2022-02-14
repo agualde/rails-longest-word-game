f31ff22c6725387b7ef076cf36cc9c98f48396f7
Andres Gualde <andres@gualde.com>
2022-02-14 20:00:57 +0100

    Attempt at calculating time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,17 +4,22 @@ require 'json'
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+    @time = Time.now
   end
 
   def score
     @guess = params[:word]
     @letters = params[:data]
+    @start_time = params[:time]
+    @end_time = Time.now
+    @result = @end_time - @start_time.to_i
 
     if @guess.chars.all? { |letter| @letters.include?(letter.upcase) }
       response = URI.open("https://wagon-dictionary.herokuapp.com/#{@guess}")
       json = JSON.parse(response.read)
       if json['found']
-        @score = "Cool guess from a cool guy, your score was: #{@guess.chars.count}"
+        @score = "Cool guess from a cool guy, your score was: #{@guess.chars.count} and your time was #{@result}"
+
       else
         @score = 'Lmao not even an english word try again bozo'
       end
